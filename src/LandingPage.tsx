61101e8121b8e6974b2a4ae25796930c8ce9c2cf
dilan besong <besongdilan@gmail.com>
2025-11-06 13:34:27 +0100

    fix image issue

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import './styles/LandingPage.css'
 
+
 export const LandingPage = () => {
      return <>
         <header>
@@ -27,7 +28,7 @@ export const LandingPage = () => {
   {/* <!-- About the Author Section --> */}
   <section className="about" id="about">
     <h2>About the Creator & Sponsor</h2>
-    <img src="../src/assets/prophet-crissin.jpg" alt="Author Image" />
+    <img src="https://i.postimg.cc/vHYnhtJ3/prophet-crissin.jpg" alt="Author Image" />
     <h3>Prophet Guy Crissin</h3>
     <p>
       Guy Crissin, the creator and sponsor of the Bible App, is a passionate believer dedicated to spreading the message of faith and love through technology. 
